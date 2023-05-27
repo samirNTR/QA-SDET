554ea57b978b030b67e2ddb0e7904d67d31f2dee
VIRAL SAMIR <127932064+samirNTR@users.noreply.github.com>
2023-05-27 12:37:35 +0530

    6th commit

---
+++ src/Testing/ArrayAddition.java
@@ -0,0 +1,29 @@
+package Testing;
+
+public class ArrayAddition {
+
+	public static int calculateSum(int[] array) {
+		int sum = 0;
+		boolean oddLength = false;
+
+		if (array.length % 2 != 0) {
+			oddLength = true;
+		}
+
+		for (int i = 0; i < array.length - 1; i += 2) {
+			sum += array[i] + array[i + 1];
+		}
+
+		if (oddLength) {
+			sum += array[array.length - 1];
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 4, 5, 8, 10 };
+		int sum = calculateSum(array);
+		System.out.println("Sum of array: " + sum);
+	}
+}
