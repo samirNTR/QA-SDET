cb9c82895563d8a4ff5275b0e1990a6b9defa660
samir <samir@LAPTOP-MQ7POIOU>
2023-06-28 20:26:06 +0530

    new commit

---
+++ src/JavaPrac1/Myprac44.java
@@ -0,0 +1,14 @@
+package JavaPrac1;
+
+public class Myprac44 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My name is java");
+		
+	
+
+	}
+
+}
