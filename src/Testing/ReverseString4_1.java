272aeded2d752747272aa99186561b3491999d97
samir <samirviral846@gmail.com>
2023-08-12 21:05:39 +0530

    new commit

--- src/Testing/ReverseString4_1.java
+++ src/Testing/ReverseString4_1.java
@@ -2,14 +2,14 @@ package Testing;
 
 // Reverse the sentence I LOVE JAVA---->JAVA LOVE I
 
-public class ReverseString4 {
+public class ReverseString4_1 {
 
 	public static void main(String[] args) {
 		String originalString = "I LOVE JAVA";
 		String[] words = originalString.split(" ");
 		String reversedString = "";
 
-		for (int i = words.length - 1; i >= 0; i--) {
+		for (int i = words.length -1; i >= 0; i--) {
 			reversedString = reversedString + words[i] + " ";
 		}
 
