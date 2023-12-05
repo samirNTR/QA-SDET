86449bfdd81fb0e8c5fed6846eea670355afabd7
samir <samirviral846@gmail.com>
2023-12-05 14:48:47 +0530

    new commit

--- src/Testing/SortedWords.java
+++ src/Testing/SortedWords.java
@@ -32,6 +32,33 @@ public class SortedWords {
          List<String> al= new ArrayList<>(hm.keySet());
           Collections.sort(al);
           System.out.println(al);
+          
+          
+          int temp=0;
+          
+          for(String word:al)
+          {
+        	  
+        	  char ch[]=word.toCharArray();
+        	  for(int i=0; i<ch.length; i++)
+        	  {
+        		  
+        		  for(int j=i+1; j<ch.length; j++)
+        		  {
+        			  
+        			  if(ch[i]>ch[j])
+        			  {
+        				  temp=ch[i];
+        				  ch[i]=ch[j];
+        				  
+        				  ch[j]=(char)temp;
+        			  }
+        		  }
+        	  }
+        	//  System.out.println(new String(ch)+" ");
+        	  System.out.println(ch+" ");
+        	  
+          }
         
     }    
     
