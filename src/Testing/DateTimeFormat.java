5fd4c02530983ebada57af3f87b8f31662fc5e85
samir <samirviral846@gmail.com>
2024-06-08 14:21:08 +0530

    NEW COMMIT

--- src/Testing/DateTimeFormat.java
+++ src/Testing/DateTimeFormat.java
@@ -1,22 +1,34 @@
 package Testing;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DateTimeFormat {
-    public static void main(String[] args) {
-        // Input date string
-        String inputDate = "8-04-2022";
+	public static void main(String[] args) {
+		// Input date string
 
-        // Parse the input string to LocalDate using a formatter
-        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("d-MM-yyyy");
-        LocalDate date = LocalDate.parse(inputDate, inputFormatter);
+		Date d = new Date();
+	
+		String inputDate = "18-04-2022";
+		
+		SimpleDateFormat sdf= new SimpleDateFormat("dd/M/yyyy");
+		SimpleDateFormat sd= new SimpleDateFormat("dd/M/yyyy  HH:mm:ss");
+		
+		System.out.println(sdf.format(d));
+		System.out.println(d.toString());
+		System.out.println(sd.format(d));
 
-        // Format LocalDate to the desired output format
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("d-MMM-yyyy");
-        String formattedDate = date.format(outputFormatter);
+		// Parse the input string to LocalDate using a formatter
+		DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDate date = LocalDate.parse(inputDate, inputFormatter);
 
-        // Print the formatted date
-        System.out.println(formattedDate);
-    }
+		// Format LocalDate to the desired output format
+		DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+		String formattedDate = date.format(outputFormatter);
+
+		// Print the formatted date
+		System.out.println(formattedDate);
+	}
 }
