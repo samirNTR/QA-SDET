a110e6087a9db5c8c7e8c9beab515ec78f3dfa0b
samir <samirviral846@gmail.com>
2025-02-21 13:03:39 +0530

    new commit

--- src/interviewExp/StringConcat.java
+++ src/interviewExp/StringConcat.java
@@ -4,34 +4,27 @@ public class StringConcat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		        String str1="SAMIR";
-		                                                           //SKAUMMIARR
-                 String str2="KUMAR";
-		        
-		        int len= Math.max(str1.length(),str2.length());
-		        
-		        StringBuilder sb= new StringBuilder();
-		        
-		        for(int i=0; i<len; i++)
-		        {
-		            
-		            if(i<str1.length())
-		            {
-		                sb.append(str1.charAt(i));
-		            }
-		            
-		            
-		            if(i<str2.length())
-		            {
-		               sb.append(str2.charAt(i)); 
-		            }
-		        }
-		        
-		        System.out.println(sb.toString());
-		    }
-		
 
+		String str1 = "SAMIR";
+		// SKAUMMIARR
+		String str2 = "KUMAR";
+
+		int len = Math.max(str1.length(), str2.length());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < len; i++) {
+
+			if (i < str1.length()) {
+				sb.append(str1.charAt(i));
+			}
+
+			if (i < str2.length()) {
+				sb.append(str2.charAt(i));
+			}
+		}
+
+		System.out.println(sb.toString());
 	}
 
-
+}
